31bc53baf01c0ad188e71a3444864e0dba5681eb
Karthik Sirasanagandla <sirkarthik@gmail.com>
2018-08-02 01:18:44 +0530

    Modelling with Decision Tree and Grid Search

---
+++ make_meshgrid.py
@@ -0,0 +1,22 @@
+# src: http://scikit-learn.org/stable/auto_examples/svm/plot_iris.html
+
+import numpy as np
+
+def make_meshgrid(x, y, h=.02):
+    """Create a mesh of points to plot in
+
+    Parameters
+    ----------
+    x: data to base x-axis meshgrid on
+    y: data to base y-axis meshgrid on
+    h: stepsize for meshgrid, optional
+
+    Returns
+    -------
+    xx, yy : ndarray
+    """
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
---
+++ plot_contours.py
@@ -0,0 +1,21 @@
+# src: http://scikit-learn.org/stable/auto_examples/svm/plot_iris.html
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm
+
+def plot_contours(ax, clf, xx, yy, **params):
+    """Plot the decision boundaries for a classifier.
+
+    Parameters
+    ----------
+    ax: matplotlib axes object
+    clf: a classifier
+    xx: meshgrid ndarray
+    yy: meshgrid ndarray
+    params: dictionary of params to pass to contourf, optional
+    """
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
